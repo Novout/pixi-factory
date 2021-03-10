6a2dfae8b29c098c7fd59256211aa5ddd0d2fc42
Novout <novout@hotmail.com>
2021-03-10 12:24:25 -0300

    chore: base types

--- src/types.ts
+++ src/types.ts
@@ -22,6 +22,16 @@ export interface PIXISprite extends PIXIDefaultSprite {
   _d20RPGPropertiesAdded?: boolean;
 }
 
+/**
+ * A velocity settings for facility sprite movement
+ *
+ * @type {Interface}
+ * @property {boolean} movement - `true` allows movement sprite
+ * @property {boolean} x - velocity in x axis
+ * @property {boolean} y - velocity in y axis
+ * @property {function} setVelocity - set value in velocity.x and velocity.y
+ * @property {function} setMovement - switch boolean value for velocity.movement
+ */
 export interface PIXISpriteVelocity {
   movement: boolean;
   x: number;
@@ -30,17 +40,91 @@ export interface PIXISpriteVelocity {
   setMovement: (sprite: PIXISprite) => void;
 }
 
+/**
+ * A options settings for create generic Factory.Sprite
+ *
+ * @type {Interface}
+ * @property {boolean} bump - `true` allows bump properties
+ * @property {boolean} velocity - `true` allows velocity properties
+ * @property {boolean} d20rpg - `true` allows generic d20 system properties
+ */
 export interface PIXISpriteOptions {
   bump?: boolean;
   velocity?: boolean;
   d20rpg?: boolean;
 }
 
+/**
+ * A content for generic d20 system
+ *
+ * @type {Interface}
+ */
 export interface PIXISpriteOpenRPG {
-  [index: string]:
-    | Record<string, Record<any, any> | string | boolean | number>
-    | string
-    | boolean
-    | Array<any>
-    | number;
+  life: PIXISpriteOpenRPGLife;
+  armor: PIXISpriteOpenRPGArmor;
+  resistance: PIXISpriteOpenRPGResistance;
+  action: PIXISpriteOpenRPGAction;
+  inventory: Array<any>;
+}
+
+export interface PIXISpriteOpenRPGLife {
+  HP: number;
+  minHP: number;
+  maxHP: number;
+  temporaryHP: number;
+}
+
+export interface PIXISpriteOpenRPGArmor {
+  CA: number;
+  minCA: number;
+  maxCA: number;
+  temporaryCA: number;
+}
+
+export interface PIXISpriteOpenRPGResistance {
+  poisoning: boolean;
+  bleeding: boolean;
+  freezing: boolean;
+  burning: boolean;
+  charmed: boolean;
+  stunned: boolean;
+  bruised: boolean;
+  imortal: boolean;
+  poisoning_value: number;
+  bleeding_value: number;
+  freezing_value: number;
+  burning_value: number;
+  charmed_value: number;
+  stunned_value: number;
+  bruised_value: number;
+  imortal_value: number;
+}
+
+export interface PIXISpriteOpenRPGAction {
+  attack: boolean;
+  attack_time: number;
+  attack_hit: boolean;
+  attack_velocity: number;
+  distance: boolean;
+  distance_time: number;
+  distance_hit: boolean;
+  interactive_inventory: boolean;
+  interactive_ui: boolean;
+  interactive_item: boolean;
+  position: PIXISpriteOpenRPGActionPosition;
+  magic: PIXISpriteOpenRPGActionMagic;
+}
+
+export interface PIXISpriteOpenRPGActionPosition {
+  x: string | number;
+  y: string | number;
+  area: boolean;
+}
+
+export interface PIXISpriteOpenRPGActionMagic {
+  magic: boolean;
+  magic_hit: boolean;
+  magic_velocity: number;
+  magic_slot_max: number;
+  magic_slot: number;
 }
