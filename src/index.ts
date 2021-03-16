bf74a547cd241d1f03559e72e5e3f9559283f4cf
Novout <novout@hotmail.com>
2021-03-16 16:52:17 -0300

    docs: module implementation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+/**
+ * @module Factory
+ */
 export * from './factory';
+
+/**
+ * @module Sprite
+ */
 export * from './sprite';
+
+/**
+ * @module Group
+ */
 export * from './group';
+
+/**
+ * @module Utils
+ */
 export * as Utils from './types';
 
 import Factory from './factory';
