40bd83acf0aa748bdb485cd6883c8e863940e059
Novout <novout@hotmail.com>
2021-04-26 15:46:32 -0300

    chore: rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,23 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import json from '@rollup/plugin-json';
 import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
 import dts from 'rollup-plugin-dts';
+import pkg from './package.json';
 
-const optionsForCompile = ['esm', 'amd'];
+const optionsForCompile = ['esm', 'cjs', 'iife'];
 
 const createPlugins = () => {
   return [typescript(), json(), resolve(), commonjs(), babel({ exclude: 'node_modules/**' })];
 };
+
+const banner = [
+  `/*!`,
+  ` * ${pkg.name} - v${pkg.version}`,
+  ` *`,
+  ` * ${pkg.name} is licensed under the MIT License.`,
+  ` * http://www.opensource.org/licenses/mit-license`,
+  ` */`,
+].join('\n');
 
 const createConfig = () => {
   const list = [];
@@ -22,24 +31,12 @@ const createConfig = () => {
         file: `./lib/index.${format}.js`,
         format,
         exports: 'named',
-      },
-      plugins: createPlugins(),
-    });
-
-    list.push({
-      input: `./src/index.ts`,
-      output: {
-        file: `./lib/bundle.${format}.min.js`,
-        format,
-        exports: 'named',
+        banner,
+        sourcemap: true,
+        name: '_pixi_factory'
       },
       plugins: [
-        ...createPlugins(),
-        terser({
-          format: {
-            comments: false,
-          },
-        }),
+        ...createPlugins()
       ],
     });
   });
